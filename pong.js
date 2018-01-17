47a7ddf70c2a9a5a5453d2c00c329c42137af40c
michalbrk <brakowski.m@gmail.com>
2018-01-17 21:56:11 +0100

    restructuring elements into the class constructor, setting boundaries and movement

--- pong.js
+++ pong.js
@@ -14,7 +14,20 @@ document.addEventListener("DOMContentLoaded", function(e) {
             this.pos = new Vec;
             this.size = new Vec(w,h);
         }
+        get left() {
+            return this.pos.x - this.size.x / 2;
+        }
+        get right() {
+            return this.pos.x + this.size.x / 2;
+        }
+        get top() {
+            return this.pos.y - this.size.y / 2;
+        }
+        get bottom() {
+            return this.pos.y + this.size.y / 2;
+        }
     }
+    
     
     //using the Rec to create the ball. The ball object will inherit from the Rec
     class Ball extends Rec {
@@ -26,57 +39,62 @@ document.addEventListener("DOMContentLoaded", function(e) {
         }
     }
     
-    let canvas = document.getElementById('pong');
-    let context = canvas.getContext('2d');
-    
-    //Creating the new ball
-    let ball = new Ball;
-    ball.pos.x = 100;
-    ball.pos.y = 50;
-    ball.vel.x = 100;
-    ball.vel.y = 100;
-    
-    let lastTime;
-    
-    
-    function callback(millsecs) {
-        if(lastTime) {
-            updates((millsecs - lastTime) / 1000)
-        }
-        lastTime = millsecs;
-        
-        //ReqAnmFrm calls a callback for the next time when the browser is ready to draw
-        //In callback there is the calculation of time, how long from the last ReqAnmFrm?
-        //ReqAnmFrm is used to get a ball's postition
-        requestAnimationFrame(callback);
-    }
+    class Pong {
+        constructor(canvas) {
+            this._canvas = canvas;
+            this._context = canvas.getContext('2d');
+            
+            //Creating the new ball
+            this.ball = new Ball;
+            this.ball.pos.x = 100;
+            this.ball.pos.y = 50;
+            this.ball.vel.x = 100;
+            this.ball.vel.y = 100;
+            
+            let lastTime;
+            let callback = (millsecs) => {
+                if(lastTime) {
+                    this.updates((millsecs - lastTime) / 1000)
+                }
+                lastTime = millsecs;
 
-    //Animating the ball, movement of the ball is relevant to the update method deltaTime
-    function updates(deltaTime) {
-        ball.pos.x += ball.vel.x * deltaTime;
-        ball.pos.y += ball.vel.y * deltaTime;
+                //ReqAnmFrm calls a callback for the next time when the browser is ready to draw
+                //In callback there is the calculation of time, how long from the last ReqAnmFrm?
+                //ReqAnmFrm is used to get a ball's postition
+                requestAnimationFrame(callback);
+            }
+            callback();
+        }
+        
+        //Animating the ball, movement of the ball is relevant to the update method deltaTime
+        updates(deltaTime) {
+        this.ball.pos.x += this.ball.vel.x * deltaTime;
+        this.ball.pos.y += this.ball.vel.y * deltaTime;
         
         //Detecting when the ball touches the screen
-        if(ball.x < 0 || ball.x > canvas.width) {
+        if(this.ball.left < 0 || this.ball.right > this._canvas.width) {
             //Here, we check it horizontally
             //Inverting the ball when it touches the screen
-            ball.vel.x = -ball.vel.x;
+            this.ball.vel.x = -this.ball.vel.x;
         }
         
         //Here, we check it vertically
-        if(ball.y < 0 || ball.y > canvas.height) {
-            ball.vel.y = -ball.vel.y;
+        if(this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
+            this.ball.vel.y = -this.ball.vel.y;
         }
     
         //Canvas properties
-        context.fillStyle = '#000';
-        context.fillRect(0,0, canvas.width, canvas.height);
+        this._context.fillStyle = '#000';
+        this._context.fillRect(0,0, this._canvas.width, this._canvas.height);
     
         //Ball properties
-        context.fillStyle = '#fff';
-        context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+        this._context.fillStyle = '#fff';
+        this._context.fillRect(this.ball.pos.x, this.ball.pos.y, this.ball.size.x, this.ball.size.y);
+        }
     }
     
-    callback();
+    let canvas = document.getElementById('pong');
+    //Initialize the game
+    let pong = new Pong(canvas);
 });
 
