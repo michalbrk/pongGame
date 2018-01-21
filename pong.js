04c8aed49ea26f4fd0bf18877e801f247b3c9bd2
michalbrk <brakowski.m@gmail.com>
2018-01-21 13:20:51 +0100

    setting up the score

--- pong.js
+++ pong.js
@@ -95,6 +95,40 @@ document.addEventListener("DOMContentLoaded", function(e) {
             }
             callback();
             
+            //Setting the score numbers
+            this.CHAR_PIXEL = 10;
+            this.CHARS = [
+                '111101101101111',
+                '010010010010010',
+                '111001111100111',
+                '111001111001111',
+                '101101111001001',
+                '111100111001111',
+                '111100111101111',
+                '111001001001001',
+                '111101111101111',
+                '111101111001111'
+                
+            ].map(str => {
+                const canvas = document.createElement('canvas');
+                canvas.height = this.CHAR_PIXEL * 5;
+                canvas.width = this.CHAR_PIXEL * 3;
+                const context = canvas.getContext('2d');
+                context.fillStyle = 'fff';
+                
+                //Iterate and create elements
+                str.split('').forEach((fill, i) => {
+                    if(fill === '1') {
+                        context.fillRect(
+                            (i % 3) * this.CHAR_PIXEL, 
+                            (i / 3 | 0) * this.CHAR_PIXEL,
+                            this.CHAR_PIXEL,
+                            this.CHAR_PIXEL);
+                    }
+                });
+                return canvas;
+            });
+            
             this.reset();
         }
         
@@ -102,11 +136,16 @@ document.addEventListener("DOMContentLoaded", function(e) {
             if(player.left < ball.right && player.right > ball.left &&
                player.top < ball.bottom && player.bottom > ball.top) {
                  
+                //Tweaking the velocity
+                const len = ball.vel.len;
+                
                 //Negating the velocity
                 ball.vel.x = - ball.vel.x;
                 
+                ball.vel.y += 300 * (Math.random() - .5);
+                
                 //Speeding up the ball's velocity
-                ball.vel.len *= 1.05;
+                ball.vel.len = len * 1.05;
             }
         }
         
@@ -117,14 +156,35 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
             this.drawRect(this.ball);
             
-            //Drawinf the players
+            //Drawing the players
             this.players.forEach(player => this.drawRect(player));
+            
+            this.drawScore();
         }
         
         drawRect(rect) {
             //Ball properties
             this._context.fillStyle = '#fff';
             this._context.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
+        }
+        
+        drawScore() {
+            const align = this._canvas.width / 3;
+            const CHAR_W = this.CHAR_PIXEL * 4;
+            this.players.forEach((player, index) => {
+                
+                //Convert into characters
+                const chars = player.score.toString().split('');
+                const offset = align * 
+                                (index + 1) - 
+                                (CHAR_W * chars.length / 2) + 
+                                this.CHAR_PIXEL / 2;
+                chars.forEach((char, pos) => {
+                    this._context.drawImage(this.CHARS[char | 0],
+                                           offset + pos * CHAR_W,
+                                           20);
+                });
+            });
         }
         
         //Make sure that the ball gests back to the canvas after going away
